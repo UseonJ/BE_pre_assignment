fd052c7751c066f2710158db29bf3203aaad9fad
UseonJ <naul630@gmail.com>
2025-03-03 20:15:26 +0900

    fix: update import directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { OrderController } from './order.controller';
+import { OrderController } from './order/order.controller';
 
 @Module({
     controllers: [OrderController]
