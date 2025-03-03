41ef5c7af6c6da32629a1b764770cc61e2ce5ccc
UseonJ <naul630@gmail.com>
2025-03-03 19:40:04 +0900

    wip: typing API requests

--- src/order.controller.ts
+++ src/order.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('order')
 export class OrderController {
@@ -8,12 +8,25 @@ export class OrderController {
     // }
 
     @Get()
-    getOrderData() {
-        return "Order data from the Memory";
+    getAllOrderDataList() {
+        return "All order data list from the Memory";
+    }
+
+    @Get('/:orderID')
+    getOrderDataByID(@Param('orderID') orderID: string) {
+        console.log(`Order ID: ${orderID}`);
+        return "Order data by ID";
+    }
+
+    @Get('/:customerID')
+    getOrderDataListByCustomerID(@Param('customerID') customerID: string) {
+        console.log(`Customer ID: ${customerID}`);
+        return "Order data list by Customer ID";
     }
 
     @Post()
-    createOrder() {
+    createOrder(@Body() body: any) {
+        console.log(body);
         return "Order created";
     }
 }
